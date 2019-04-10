0b93ab6c44584415c2008e9bd7ee46357c7f666c
chenjr15 <icjr@qq.com>
2019-04-10 14:53:23 +0800

    change to runtime init

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+content=$(ls -A /blog)
+if [ -z $content ] ;then
+    # empty blog folder
+    echo Initing Hexo...
+    hexo init /blog 
+else
+    echo "Skipping init."
+fi
+cd /blog && hexo generate
 nginx
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log
